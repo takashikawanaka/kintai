ac1f421702fb15b444513a293e5a3c81e1eec116
takashikawanaka <takashi.soccer.a@gmail.com>
2024-01-01 20:20:38 +0900

    Connect DB

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,46 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>
+    <div v-for="item in json" :key="item.Id">
+      <p>Id: {{ item.Id }}</p>
+      <p>Name: {{ item.Name }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
-  name: "App",
   data() {
     return {
-      value: "World"
+      json: [],
     };
-  }
+  },
+  methods: {
+    requestPost() {
+      const query = `
+    {
+      people {
+        items {
+          Id
+          Name
+        }
+      }
+    }`;
+
+      const endpoint = "/data-api/graphql";
+      fetch(endpoint, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query: query }),
+      })
+        .then((response) => response.json())
+        .then((data) => (this.json = data.data.people.items))
+        .then(() => console.log(this.json));
+    },
+  },
+  mounted() {
+    this.requestPost();
+  },
 };
 </script>
